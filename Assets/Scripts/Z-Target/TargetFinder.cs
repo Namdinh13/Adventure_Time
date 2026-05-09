ae4bda2821840fa0c6a5306a5ae10f8ca5334f34
Namdinh13 <abct53085@gmail.com>
2026-05-10 00:16:18 +0700

    Add Z-Targeting

--- Assets/Scripts/Z-Target/TargetFinder.cs
+++ Assets/Scripts/Z-Target/TargetFinder.cs
@@ -1,18 +1,31 @@
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class TargetFinder : MonoBehaviour
 {
     public static List<Transform> pool;
+    public static DistanceClass dc; 
+
+    private Transform currentTarget;
+    [SerializeField] private CinemachineCamera playerCamera;
+    [SerializeField] private CinemachineCamera targetCamera;
+    [SerializeField] private Animator targetAnimator;
+
+
     public List<Transform> poolView;
 
+    private bool lockedOn;
+    private bool coolDown;
+
     #region Input
-    private DefaultInputActions input;
+    private InputSystem_Actions input;
 
     private void Awake()
     {
-        input = new DefaultInputActions();
+        input = new InputSystem_Actions();
     }
 
     private void OnEnable()
@@ -26,19 +39,148 @@ public class TargetFinder : MonoBehaviour
     }
     #endregion Input
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         if (pool == null)
         {
             pool = new List<Transform>();
         }
-    }
 
-    // Update is called once per frame
+        if (dc == null)
+        {
+            dc = new DistanceClass();
+        }
+    }
+   
     void Update()
     {
         poolView = pool;
+
+        //if (input.CameraControls.Targeting.IsPressed() && !lockedOn)
+        //{
+        //    LockOn();
+        //}
+        //else if (!input.CameraControls.Targeting.IsPressed())
+        //{
+        //    LockOff();
+        //}
+
+        if (input.CameraControls.Targeting.WasPressedThisFrame())
+        {
+            if (!lockedOn)
+            {
+                LockOn();
+            }
+            else
+            {
+                LockOff();
+            }
+        }
+
+
+        if (lockedOn)
+        {
+            if (input.Player.Look.ReadValue<Vector2>().x > 5.0f)
+            {
+                SelectTarget(1);
+            }
+            else if (input.Player.Look.ReadValue<Vector2>().x <= -5.0f)
+            {
+                SelectTarget(-1);
+            }
+            else
+            {
+
+            }
+        } 
+    }
+
+    private void LockOn()
+    {
+        currentTarget = NearestTarget();
+
+        if (currentTarget)
+        {
+            targetCamera.LookAt = currentTarget;
+            targetCamera.gameObject.SetActive(true);
+
+            playerCamera.gameObject.SetActive(false);
+            lockedOn = true;
+            targetAnimator.SetBool("lockedOn", true);
+        }
+    }
+
+    private void LockOff()
+    {
+        currentTarget = null;
+
+        targetCamera.LookAt = null;
+        targetCamera.gameObject.SetActive(false);
+
+        playerCamera.gameObject.SetActive(true);
+        lockedOn = false;
+        targetAnimator.SetBool("lockedOn", false);
+    }
+
+    private void SelectTarget(int next)
+    {
+        if (!coolDown && pool != null && pool.Count > 1)
+        {
+            System.Predicate<Transform> predicate = FindTransform;
+            int currentIndex = pool.FindIndex(predicate);
+
+            int nextIndex = currentIndex + next;
+
+            if (nextIndex >= 0 && nextIndex < pool.Count) 
+            {
+                currentTarget = pool[nextIndex];
+                targetCamera.LookAt = currentTarget;
+                coolDown = true;
+                StartCoroutine("ResetCoolDown");
+
+            }
+        }
+    }
+
+    private bool FindTransform(Transform t)
+    {
+        return t.Equals(currentTarget);
+    }
+
+    IEnumerator ResetCoolDown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        coolDown = false;
+    }
+
+    private Transform NearestTarget()
+    {
+        if (pool != null && pool.Count > 0)
+        {
+            Vector3 center = new Vector3(0.5f, 0.5f, 0);
+            Camera cam = Camera.main;
+
+            int minIndex = 0;
+            float shortestDistance = 1000.0f;
+
+            for (int i = 0; i < pool.Count; i++)
+            {
+                Vector3 targetViewport = cam.WorldToViewportPoint(pool[i].position);
+                targetViewport -= Vector3.forward * targetViewport.z;
+
+                float targetDistanceFromCenter = Vector3.Distance(center, targetViewport);
+
+                if (targetDistanceFromCenter < shortestDistance)
+                {
+                    shortestDistance = targetDistanceFromCenter;
+                    minIndex = i;
+                }
+
+            }
+            //cam.WorldToViewportPoint(pool[1].position);
+            return pool[minIndex];
+        }
+        return null;
     }
 
     public static void AddToPool(Transform target)
@@ -46,6 +188,7 @@ public class TargetFinder : MonoBehaviour
         if (pool != null && !pool.Contains(target))
         {
             pool.Add(target);
+            pool.Sort(dc);
         }
     }
 
@@ -54,6 +197,31 @@ public class TargetFinder : MonoBehaviour
         if (pool != null && pool.Contains(target))
         {
             pool.Remove(target);
+            pool.Sort(dc);
+        }
+    }
+}
+
+public class DistanceClass : IComparer<Transform>
+{
+    public int Compare(Transform x, Transform y)
+    {
+        Camera cam = Camera.main;
+
+        Vector3 xViewport = cam.WorldToViewportPoint(x.position);
+        Vector3 yViewport = cam.WorldToViewportPoint(y.position);
+
+        if (xViewport.x < yViewport.x)
+        {
+            return -1;
+        }
+        else if (xViewport.x == yViewport.x)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
         }
     }
 }
